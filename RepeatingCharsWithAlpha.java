b83558e5e234ecdd96baff4c27b9ab15c732f889
Vinay Sharma <65557772+Vinay08sharma@users.noreply.github.com>
2022-05-11 00:53:34 +0530

    Update RepeatingCharsWithAlpha.java

--- RepeatingCharsWithAlpha.java
+++ RepeatingCharsWithAlpha.java
@@ -1,22 +1,6 @@
 public class RepeatingCharsWithAlpha {
     public static void main(String[] args) {
 
-//        String input = "aaabbccda";
-//        int count = 1;
-//        char temp = input.charAt(0);
-//        for(int i=1;i<input.length();i++)
-//        {
-//            if(input.charAt(i)==temp)
-//                count++;
-//            else
-//            {
-//                System.out.print(temp+""+count);
-//                count = 1;
-//                temp = input.charAt(i);
-//            }
-//        }
-//        System.out.print(temp+""+count);
-
         String s = "aabcddeebbb";
 
         int count = 1;
